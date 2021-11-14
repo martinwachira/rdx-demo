885f14179e7917b88b94b8e1ad74b89adb4f31c3
martin-wachira <martinnwachira@gmail.com>
2021-11-14 09:17:44 +0300

    ---test subscibe method

--- redux-demo.js
+++ redux-demo.js
@@ -3,3 +3,7 @@ const redux = require("redux");
 const counterReducer = () => {};
 
 const store = redux.createStore(counterReducer);
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
