499e037b2f4577e9eac9174db7a57b3aa4e78824
martinwachira <martinnwachira@gmail.com>
2021-11-17 12:27:21 +0300

    init redux index file

---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import { createStore } from "redux";
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "INC") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "DEC") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+};
+
+const store = createStore(counterReducer);
+
+export default store;
