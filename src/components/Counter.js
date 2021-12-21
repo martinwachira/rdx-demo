dd96ab3d67968e1e996aad96d6da37b15268edfe
martin-wachira <martinnwachira@gmail.com>
2021-12-21 13:31:40 +0300

    multi state properties

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,6 +5,7 @@ import classes from "./Counter.module.css";
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state?.counter);
+  const show = useSelector((state) => state?.showCounter);
 
   const incrementHandler = () => {
     dispatch({ type: "INC" });
@@ -18,13 +19,13 @@ const Counter = () => {
     dispatch({ type: "DEC" });
   };
   const toggleCounterHandler = () => {
-    console.log("counter", counter);
+    dispatch({ type: "TOGGLE" });
   };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>--{counter}--</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={incByHandler}>Increase By 5</button>
--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,30 @@
 import { createStore } from "redux";
 
+const initState = { counter: 0, showCounter: true };
 //created the reducer counter
-const counterReducer = (state = { counter: 0 }, action) => {
+const counterReducer = (state = initState, action) => {
   if (action.type === "INC") {
     return {
       counter: state.counter + 1,
+      showCounter: state.showCounter,
     };
   }
   if (action.type === "INCBY") {
     return {
       counter: state.counter + action.value,
+      showCounter: state.showCounter,
     };
   }
   if (action.type === "DEC") {
     return {
       counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  }
+  if (action.type === "TOGGLE") {
+    return {
+      showCounter: !state.showCounter,
+      counter: state.counter,
     };
   }
 };
