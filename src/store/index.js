368230ed8f45588a208da85a51e1883d831ef7ae
martin-wachira <martinnwachira@gmail.com>
2021-12-22 21:09:23 +0300

    use redux toolkit createSlice method to simplify the code

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
 import { createStore } from "redux";
 
 const initState = { counter: 0, showCounter: true };
+
+// use redux toolkit createSlice method to simplify the code
+createSlice({
+  name: "counter",
+  initState,
+  reducers: {
+    inc: (state) => {
+      state.counter = +1;
+    },
+    dec: (state) => {
+      state.counter = -1;
+    },
+    increase: (state, action) => {
+      state.counter = state.counter + action.value;
+    },
+    toggle: (state) => {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
 //created the reducer counter
 const counterReducer = (state = initState, action) => {
   if (action.type === "INC") {
