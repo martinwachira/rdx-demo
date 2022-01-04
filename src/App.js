73581ebd60d0bfc876aa34fafa38ff0829b858a8
martin-wachira <martinnwachira@gmail.com>
2022-01-04 14:30:24 +0300

    intro multi auth and header comps

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import Auth from "./components/Auth";
 import Counter from "./components/Counter";
+import Header from "./components/Header";
+import { React } from "react";
 
 function App() {
-  return <Counter />;
+  return (
+    <>
+      <Header />
+      <Auth />
+      <Counter />
+    </>
+  );
 }
 
 export default App;
