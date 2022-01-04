7d93a0512e8db102a5ae472b4e650dbf7058399f
martin-wachira <martinnwachira@gmail.com>
2022-01-04 17:15:04 +0300

    init redux-side effects part 1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-import Auth from "./components/Auth";
-import Counter from "./components/Counter";
-import Header from "./components/Header";
-import { React } from "react";
-import UserProfile from "./components/UserProfile";
-import { useSelector } from "react-redux";
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
 
 function App() {
-  const isAuth = useSelector((state) => state?.auth.isAuth);
   return (
-    <>
-      <Header />
-      {!isAuth && <Auth />}
-      {isAuth && <UserProfile />}
-      <Counter />
-    </>
+    <Layout>
+      <Cart />
+      <Products />
+    </Layout>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
-import "./index.css";
-
-import App from "./App";
-import { Provider } from "react-redux";
-import React from "react";
 import ReactDOM from "react-dom";
-import store from "./store/index";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
