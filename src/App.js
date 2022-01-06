69db0913e2098d46c9ece955ecae67d4bca5c0bc
martin-wachira <martinnwachira@gmail.com>
2022-01-06 13:23:23 +0300

    get overall cart state in main app comp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 function App() {
   const showCart = useSelector((state) => state.cartUI.showCart);
+  const cart = useSelector((state) => state.cart);
   return (
     <Layout>
       {showCart && <Cart />}
