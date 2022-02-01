030b5c606e97cc3caac7081442d1a131b24d747f
martin-wachira <martinnwachira@gmail.com>
2022-02-02 00:25:14 +0300

    small student grading method

--- secondary.js
+++ secondary.js
@@ -4,4 +4,29 @@ import Second from "./test-leet3.js";
 First();
 Second();
 
-console.log("sdhfshkf");
+// console.log("sdhfshkf");
+
+/**
+ * Grading method
+ */
+const grades = [73, 67, 38, 33];
+
+function gradding(grades) {
+  // Write your code here
+  for (var i = 0; i < grades.length; i++) {
+    let m = 0;
+    if (grades[i] >= 38) {
+      m = Math.ceil(grades[i] / 5) * 5;
+      if (m - grades[i] < 3) {
+        console.log("Pass", (grades[i] = m));
+      } else {
+        console.log("Pass", grades[i]);
+      }
+    } else {
+      console.log("Fail", grades[i]);
+    }
+  }
+  return grades;
+}
+
+gradding(grades);
