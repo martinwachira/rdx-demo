b702057622a3ac13f88f83f2134cc33c6177d0fe
Martin Wachira <60017194+martinwachira@users.noreply.github.com>
2022-02-10 14:46:04 +0300

    init code

--- test-leet.js
+++ test-leet.js
@@ -8,4 +8,41 @@ You may assume the two numbers do not contain any leading zero, except the numbe
 // Output should be : [7,0,8]
 // Explanation: 342 + 465 = 807.
 
+//create a solution to solve this problem
 
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
+// create a function to solve this problem
+
+var addTwoNumbers = function (l1, l2) {
+  // create a variable to store the sum of the two numbers
+  let sum = 0;
+  let result = new ListNode(0);
+  let curr = result;
+  while (l1 || l2 || sum) {
+    sum += (l1 ? l1.val : 0) + (l2 ? l2.val : 0);
+    curr.next = new ListNode(sum % 10);
+    curr = curr.next;
+    sum = Math.floor(sum / 10);
+    l1 = l1 ? l1.next : l1;
+    l2 = l2 ? l2.next : l2;
+  }
+  // return the sum of the two numbers
+  return result.next;
+};
+
+// linked list
+let n1 = {
+  data: 1,
+};
+let n2 = {
+  data: 2,
+};
+
+n1.next = n2;
+
+console.log("Thee output:", n1);
